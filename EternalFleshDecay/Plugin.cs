389356125ae4b8ea821a5d520c1d1c8fb4b2af98
intjiraya <177346249+intjiraya@users.noreply.github.com>
2024-08-26 17:20:04 +0300

    Timer display fix

--- EternalFleshDecay/Plugin.cs
+++ EternalFleshDecay/Plugin.cs
@@ -2,6 +2,9 @@
 using Exiled.Events.Handlers;
 using Exiled.Events.EventArgs.Scp3114;
 using Exiled.API.Enums;
+using System.Dynamic;
+using InventorySystem.Items.Coin;
+using Exiled.Events.EventArgs.Player;
 
 namespace EternalFleshDecay
 {
@@ -11,29 +14,37 @@ namespace EternalFleshDecay
         public override string Name { get; } = "EternalFleshDecay";
         public override string Prefix { get; } = "EternalFleshDecay";
 
-        public override System.Version Version { get; } = new System.Version(1, 0, 0);
-        public override System.Version RequiredExiledVersion { get; } = new System.Version(8, 8, 0);
+        public override System.Version Version { get; } = new System.Version(1, 0, 1);
+        public override System.Version RequiredExiledVersion { get; } = new System.Version(8, 0, 0);
         public override PluginPriority Priority { get; } = PluginPriority.Medium;
 
         public override void OnEnabled()
         {
-            // Subscribing to the `Scp3114.Disguised` event to handle disguise logic
+            Scp3114.Disguising += OnDisguising;
             Scp3114.Disguised += OnDisguised;
+            Scp3114.Revealed += OnRevealed;
+
             base.OnEnabled();
         }
 
         public override void OnDisabled()
         {
-            // Unsubscribing from the `Scp3114.Disguised` event to prevent memory leaks
+            Scp3114.Disguising -= OnDisguising;
             Scp3114.Disguised -= OnDisguised;
+
             base.OnDisabled();
         }
 
-        // Event handler for when SCP-3114 is disguised
+        private void OnDisguising(DisguisingEventArgs ev)
+        {
+            ev.Scp3114.DisguiseDuration = Config.DisguiseDuration;
+            ev.Scp3114.Identity.ServerResendIdentity();
+        }
+
         private void OnDisguised(DisguisedEventArgs ev)
         {
-            // Setting the disguise duration based on the configuration
             ev.Scp3114.DisguiseDuration = Config.DisguiseDuration;
+            ev.Scp3114.Identity.ServerResendIdentity();
         }
     }
 }
